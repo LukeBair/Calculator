9f40a77f6e87988c348a9583ce634162d5d28afb
Luke Bair <lukebair@me.com>
2022-07-16 15:27:59 -0700

    tree
    wip

---
+++ src/test/java/TreeTest.java
@@ -0,0 +1,8 @@
+import tree.GenericTree;
+import tree.GenericTree.Node;
+
+public class TreeTest {
+    public void TestTheTree() {
+//        GenericTree<String> tree = new GenericTree<>();
+    }
+}
